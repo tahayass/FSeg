0aae58b1e972bf3089fafd6289c8f046aed7cbe3
tahayass <tahayassinetizaoui@insat.u-carthage.tn>
2023-09-06 18:52:58 +0100

    function to generate prediction for all dataset

--- FoodAreaSegmentation/main.py
+++ FoodAreaSegmentation/main.py
@@ -3,9 +3,74 @@ import torch
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 from sam_model import LoadSAMPredictor,GenerateMask
 from utils import show_box,show_mask
+
+from annotation_scripts.ExtractBoundingBox import load_coco_data, get_bounding_boxes_for_all_image, get_image_info
+
+def format_bbox(input_bbox):
+    #format the bounding box values to sam model bbox specifications
+    return [input_bbox[0], input_bbox[1], input_bbox[0]+input_bbox[2], input_bbox[1]+input_bbox[3]]
+
+
+
+def GenerateMasksForDataset(coco_dataset_path,dataset_name=''):
+
+    experiment_path = os.path.join('.','FoodAreaSegmentation','experiments',dataset_name)
+
+    if os.path.exists(experiment_path) == False:
+        os.mkdir(os.path.join('.','FoodAreaSegmentation','experiments',dataset_name))
+
+    annotation_file_path = os.path.join(coco_dataset_path,'_annotations.coco.json')
+
+    coco, category_id_to_name = load_coco_data(annotation_file_path)
+
+    image_annotation = get_bounding_boxes_for_all_image(coco,category_id_to_name)
+
+    image_ids = coco.getImgIds()
+
+    for id in tqdm(image_ids):
+
+        image = cv2.imread(os.path.join(coco_dataset_path,get_image_info(coco, id)['file_name']))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        masks = GenerateMaskForImage(image, bounding_boxes=image_annotation[id])
+
+        plt.figure(figsize=(10, 10))
+        plt.imshow(image)
+        for i,mask in enumerate(masks):
+            show_mask(mask[0], plt.gca())
+            category_name = image_annotation[id][i]['category_name']
+            show_box(format_bbox(image_annotation[id][i]['bbox']), plt.gca(),category_name=category_name)
+        plt.savefig(os.path.join(experiment_path,f'{id}.jpg'))
+
+
+def GenerateMaskForImage(image, bounding_boxes=[]):
+
+    masks=[]
+    #Loading SAM predictor
+    sam_predictor = LoadSAMPredictor(sam_checkpoint=SAM_CHECKPOINT,model_type=MODEL_TYPE,device='cpu')
+
+    #Create SAM embeddings for the image
+    sam_predictor.set_image(image)
+
+    #Generate mask
+    if len(bounding_boxes) == 1 :
+        input_bbox = np.array(format_bbox(bounding_boxes[0]['bbox']))
+        mask = GenerateMask(sam_predictor,input_box=input_bbox)
+        masks.append(mask)
+
+    elif len(bounding_boxes) > 1 :
+        for i in range(len(bounding_boxes)):
+            input_bbox = np.array(format_bbox(bounding_boxes[i]['bbox']))
+            mask = GenerateMask(sam_predictor,input_box=input_bbox)
+            masks.append(mask)
+
+    return masks
+
+
 
 
 
@@ -14,31 +79,8 @@ if __name__=='__main__':
     SAM_CHECKPOINT = os.path.join('.','FoodAreaSegmentation','sam_vit_h_4b8939.pth')
     MODEL_TYPE = "vit_h"
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    INPUT_BOX = np.array([66,219,66+520,219+317]) #get bbox from embeddinfs
 
-    test_image = cv2.imread(r'.\Data\Fseg-food-detection-1\train\9_jpg.rf.05b03592b319ba209316ff6cf27fd6c9.jpg')
-    test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB)
-
-    #Loading SAM predictor
-    print("Loading SAM predictor")
-    sam_predictor = LoadSAMPredictor(sam_checkpoint=SAM_CHECKPOINT,model_type=MODEL_TYPE,device=DEVICE)
-
-    #Create SAM embeddings for the image
-    print("Creating image embedding")
-    sam_predictor.set_image(test_image)
-
-    #Generate mask
-    print("Generating food mask")
-    masks = GenerateMask(sam_predictor,input_box=INPUT_BOX)
-
-
-    #Show result 
-    category_name = 'seasoned spinach'
-
-
-    plt.figure(figsize=(10, 10))
-    plt.imshow(test_image)
-    show_mask(masks[0], plt.gca())
-    show_box(INPUT_BOX, plt.gca(),category_name=category_name)
-    plt.axis('off')
-    plt.show()
+    coco_dataset_path = r'.\Data\Fseg-food-detection-1\test'
+    
+    GenerateMasksForDataset(coco_dataset_path,dataset_name='test')
+    

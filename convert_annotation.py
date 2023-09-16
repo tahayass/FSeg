705f6d04cf4c930a948896fb11d446583947d6ab
SarahBenabdallah <sarah.benabdallah@planet.tn>
2023-09-16 12:07:07 +0100

    convert annotation

--- convert_annotation.py
+++ convert_annotation.py
@@ -40,90 +40,14 @@ def convert_coco_to_yolo(coco_json, output_dir):
         # Write to output file
         with open(file_name, 'a') as f:
             f.write(f"{category_id} {center_x} {center_y} {w_norm} {h_norm}\n")
+            
 
 
 if __name__ == '__main__':
     convert_coco_to_yolo(
-        './Dataset/Braised Bean Curd, Braised Potatoes, Pan-fried Battered Zucchini.v10i.coco/test/_annotations.coco.json',
-        './Dataset/Braised Bean Curd, Braised Potatoes, Pan-fried Battered Zucchini.v10i.coco/test/labels')
+        './Combined_dataset/train/_annotations.coco.json',
+        './Combined_dataset/train/labels')
     convert_coco_to_yolo(
-        './Dataset/Braised Peanuts, Braised Lotus Roots.v11i.coco/train/_annotations.coco.json',
-        './Dataset/Braised Peanuts, Braised Lotus Roots.v11i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Braised Peanuts, Braised Lotus Roots.v11i.coco/test/_annotations.coco.json',
-        './Dataset/Braised Peanuts, Braised Lotus Roots.v11i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Bulgogi, Grilled Pork Belly, Grilled Eel, Grilled Clams, grilled yellow corvina, smoked duck, Seasoned Fried Chicken, Pizza.v17i.coco/train/_annotations.coco.json',
-        './Dataset/Bulgogi, Grilled Pork Belly, Grilled Eel, Grilled Clams, grilled yellow corvina, smoked duck, Seasoned Fried Chicken, Pizza.v17i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Bulgogi, Grilled Pork Belly, Grilled Eel, Grilled Clams, grilled yellow corvina, smoked duck, Seasoned Fried Chicken, Pizza.v17i.coco/test/_annotations.coco.json',
-        './Dataset/Bulgogi, Grilled Pork Belly, Grilled Eel, Grilled Clams, grilled yellow corvina, smoked duck, Seasoned Fried Chicken, Pizza.v17i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Cucumber Kimchi.v3i.coco/train/_annotations.coco.json',
-        './Dataset/Cucumber Kimchi.v3i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Cucumber Kimchi.v3i.coco/test/_annotations.coco.json',
-        './Dataset/Cucumber Kimchi.v3i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Eoyonam\'s Stir-fried dried squid with Red Chili Paste, Stir-fried Rice Cake (non noodle), Stir-fried Rice Cake, Rice Cake Skewers.v8i.coco/train/_annotations.coco.json',
-        './Dataset/Eoyonam\'s Stir-fried dried squid with Red Chili Paste, Stir-fried Rice Cake (non noodle), Stir-fried Rice Cake, Rice Cake Skewers.v8i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Eoyonam\'s Stir-fried dried squid with Red Chili Paste, Stir-fried Rice Cake (non noodle), Stir-fried Rice Cake, Rice Cake Skewers.v8i.coco/test/_annotations.coco.json',
-        './Dataset/Eoyonam\'s Stir-fried dried squid with Red Chili Paste, Stir-fried Rice Cake (non noodle), Stir-fried Rice Cake, Rice Cake Skewers.v8i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Grilled Cutlassfish, Grilled Mackerel, Grilled Beef TripeGrilled Pork Tripe.v15i.coco/train/_annotations.coco.json',
-        './Dataset/Grilled Cutlassfish, Grilled Mackerel, Grilled Beef TripeGrilled Pork Tripe.v15i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Grilled Cutlassfish, Grilled Mackerel, Grilled Beef TripeGrilled Pork Tripe.v15i.coco/test/_annotations.coco.json',
-        './Dataset/Grilled Cutlassfish, Grilled Mackerel, Grilled Beef TripeGrilled Pork Tripe.v15i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Kimchi Pancake, Green Onion Pancake.v9i.coco/train/_annotations.coco.json',
-        './Dataset/Kimchi Pancake, Green Onion Pancake.v9i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Kimchi Pancake, Green Onion Pancake.v9i.coco/test/_annotations.coco.json',
-        './Dataset/Kimchi Pancake, Green Onion Pancake.v9i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Kimchi Pancake, Green Onion Pancake.v9i.coco/train/_annotations.coco.json',
-        './Dataset/Kimchi Pancake, Green Onion Pancake.v9i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Leaf Mustard Kimchi, Radish kimchi, Julienne Radish Fresh Salad, Kimchi, White Kimchi, Chive Kimchi.v1i.coco/train/_annotations.coco.json',
-        './Dataset/Leaf Mustard Kimchi, Radish kimchi, Julienne Radish Fresh Salad, Kimchi, White Kimchi, Chive Kimchi.v1i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Leaf Mustard Kimchi, Radish kimchi, Julienne Radish Fresh Salad, Kimchi, White Kimchi, Chive Kimchi.v1i.coco/test/_annotations.coco.json',
-        './Dataset/Leaf Mustard Kimchi, Radish kimchi, Julienne Radish Fresh Salad, Kimchi, White Kimchi, Chive Kimchi.v1i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Pickled Perilla Leaf, Braised Beans, Stir-fried Eggplant, Bracken Salad.v5i.coco/train/_annotations.coco.json',
-        './Dataset/Pickled Perilla Leaf, Braised Beans, Stir-fried Eggplant, Bracken Salad.v5i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Pickled Perilla Leaf, Braised Beans, Stir-fried Eggplant, Bracken Salad.v5i.coco/test/_annotations.coco.json',
-        './Dataset/Pickled Perilla Leaf, Braised Beans, Stir-fried Eggplant, Bracken Salad.v5i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Potato Pancake.v2i.coco/train/_annotations.coco.json',
-        './Dataset/Potato Pancake.v2i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Potato Pancake.v2i.coco/test/_annotations.coco.json',
-        './Dataset/Potato Pancake.v2i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Spicy Stir-fried Chicken, Grilled Ribs, Grilled Short Rib Patties.v16i.coco/train/_annotations.coco.json',
-        './Dataset/Spicy Stir-fried Chicken, Grilled Ribs, Grilled Short Rib Patties.v16i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Spicy Stir-fried Chicken, Grilled Ribs, Grilled Short Rib Patties.v16i.coco/test/_annotations.coco.json',
-        './Dataset/Spicy Stir-fried Chicken, Grilled Ribs, Grilled Short Rib Patties.v16i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Stir-fried Seaweed Stems, Mung Bean Sprout Salad, Spinach Salad, Stir-fried Zucchini.v6i.coco/train/_annotations.coco.json',
-        './Dataset/Stir-fried Seaweed Stems, Mung Bean Sprout Salad, Spinach Salad, Stir-fried Zucchini.v6i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Stir-fried Seaweed Stems, Mung Bean Sprout Salad, Spinach Salad, Stir-fried Zucchini.v6i.coco/test/_annotations.coco.json',
-        './Dataset/Stir-fried Seaweed Stems, Mung Bean Sprout Salad, Spinach Salad, Stir-fried Zucchini.v6i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Stir-fried Shishito Peppers, seasoned balloon flower roots, Acorn Jelly Salad, Bean Sprout Salad, Stir-fried Potatoes.v7i.coco/train/_annotations.coco.json',
-        './Dataset/Stir-fried Shishito Peppers, seasoned balloon flower roots, Acorn Jelly Salad, Bean Sprout Salad, Stir-fried Potatoes.v7i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Stir-fried Shishito Peppers, seasoned balloon flower roots, Acorn Jelly Salad, Bean Sprout Salad, Stir-fried Potatoes.v7i.coco/test/_annotations.coco.json',
-        './Dataset/Stir-fried Shishito Peppers, seasoned balloon flower roots, Acorn Jelly Salad, Bean Sprout Salad, Stir-fried Potatoes.v7i.coco/test/labels')
-    convert_coco_to_yolo(
-        './Dataset/Whole Radish Kimchi.v4i.coco/train/_annotations.coco.json',
-        './Dataset/Whole Radish Kimchi.v4i.coco/train/labels')
-    convert_coco_to_yolo(
-        './Dataset/Whole Radish Kimchi.v4i.coco/test/_annotations.coco.json',
-        './Dataset/Whole Radish Kimchi.v4i.coco/test/labels')
+        './Combined_dataset/test/_annotations.coco.json',
+        './Combined_dataset/test/labels')
+

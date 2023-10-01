c1f5c44de8b3772b113484e557257b70c166c4c7
tahayass <tahayassinetizaoui@insat.u-carthage.tn>
2023-10-01 20:02:43 +0100

    pixel count added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import os
 import cv2
+import numpy as np
 
 from FoodAreaSegmentation.sam_model import GenerateMaskForImage
 
@@ -18,7 +19,10 @@ def remove_background(image,
 def get_food_bboxes(image,
                     model,
                     resize=None):
-    return None
+    bboxes = None
+    food_types = None 
+
+    return bboxes,food_types
 
 def get_food_masks(image,
                    bboxes,
@@ -32,8 +36,28 @@ def get_food_masks(image,
 
 def calculate_surface_area(image,
                            masks,
-                           bboxes):
-    return None
+                           bboxes,
+                           food_types):
+    # Create a dictionary to store the sums of masks with the same name
+    mask_dict = {}
+
+    # Iterate over each mask and its corresponding name
+    for mask, name in zip(masks, food_types):
+        if name not in mask_dict:
+            mask_dict[name] = mask*1
+        else:
+            mask_dict[name] += mask*1
+
+    # Create a dictionary to store the count of ones in each array
+    pixel_count = {}
+
+    # Iterate over the dictionary items and sum in each mask
+    for name, summed_mask in mask_dict.items():
+        non_zero_count = np.sum(summed_mask)
+        pixel_count[name] = non_zero_count
+
+    return pixel_count
+
 
 
 if __name__=='__main__':
@@ -64,7 +88,7 @@ if __name__=='__main__':
                       diameter)
     
     #Detect food and classify them
-    bboxes = get_food_bboxes(image,
+    bboxes,food_types = get_food_bboxes(image,
                     food_detection_model,
                     resize=None)
     
@@ -78,7 +102,8 @@ if __name__=='__main__':
     #Calculates masks pixel count and returns a dictionnary with surface area for every food {'food_type':pixel_count}
     areas = calculate_surface_area(image,
                                    masks,
-                                   bboxes)
+                                   bboxes,
+                                   food_types)
     
 
     

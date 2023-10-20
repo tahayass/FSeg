0c3c5b746882198923f34e74108ed7745dce6ec1
SarahBenabdallah <sarah.benabdallah@planet.tn>
2023-10-20 11:14:10 +0100

    update main

--- main.py
+++ main.py
@@ -283,44 +283,39 @@ def calculate_surface_area(image,
 
     return pixel_count
 
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='PlateDetection/yolov5s.pt', help='model path or triton URL')
-    parser.add_argument('--source', type=str, default='PlateDetection/data/images', help='file/dir/URL/glob/screen/0(webcam)')
-    parser.add_argument('--data', type=str, default='PlateDetection/data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-csv', action='store_true', help='save results in CSV format')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='PlateDetection/runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    print_args(vars(opt))
-    return opt
 
-def get_food_bboxes_worker(opt):
+def get_food_bboxes_worker():
     global bboxes_result
-    bboxes_result = get_food_bboxes(**vars(opt))
+    bboxes_result = get_food_bboxes(
+        weights=opt["weights"],
+        source=opt["source"],
+        data=opt["data"],
+        imgsz=opt["imgsz"],
+        conf_thres=opt["conf_thres"],
+        iou_thres=opt["iou_thres"],
+        max_det=opt["max_det"],
+        device=opt["device"],
+        view_img=opt["view_img"],
+        save_txt=opt["save_txt"],
+        save_csv=opt["save_csv"],
+        save_conf=opt["save_conf"],
+        save_crop=opt["save_crop"],
+        nosave=opt["nosave"],
+        classes=opt["classes"],
+        agnostic_nms=opt["agnostic_nms"],
+        augment=opt["augment"],
+        visualize=opt["visualize"],
+        update=opt["update"],
+        project=opt["project"],
+        name=opt["name"],
+        exist_ok=opt["exist_ok"],
+        line_thickness=opt["line_thickness"],
+        hide_labels=opt["hide_labels"],
+        hide_conf=opt["hide_conf"],
+        half=opt["half"],
+        dnn=opt["dnn"],
+        vid_stride=opt["vid_stride"]
+    )
     bboxes_done_event.set()
 
 def prepare_image_embeddings_worker(image):
@@ -331,20 +326,21 @@ def prepare_image_embeddings_worker(image):
 
 
 def main(opt):
-
     #Constant variables
     SAM_CHECKPOINT = os.path.join('.','FoodAreaSegmentation','sam_vit_h_4b8939.pth')
     MODEL_TYPE = "vit_h"
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    image = cv2.imread(opt.source)
+    image = cv2.imread(opt["source"])
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     #image = cv2.resize(image,(640,640))
 
     check_requirements('PlateDetection/requirements.txt', exclude=('tensorboard', 'thop'))
 
     # Create two threads to run get_food_bboxes and prepare_image_embeddings concurrently
-    get_bboxes_thread = Thread(target=get_food_bboxes_worker, args=(opt,))
+    #get_bboxes_thread = Thread(target=get_food_bboxes_worker, args=(opt,))
+    get_bboxes_thread = Thread(target=get_food_bboxes_worker)
+
     prepare_embeddings_thread = Thread(target=prepare_image_embeddings_worker, args=(image,))
 
     # Start the threads
@@ -394,20 +390,40 @@ def main(opt):
 
 if __name__ == '__main__':
     start_time = time.time()
-
-    opt = parse_opt()
+    
+    # Define Arguments of Food Detection
+    opt = {
+        "weights": "C:/Users/Sarah Benabdallah/Documents/GitHub/FSeg/PlateDetection/best86yolovm.pt",
+        "source": "C:/Users/Sarah Benabdallah/Documents/GitHub/FSeg/test_dataset/images/test/1accbbf4-19f8-4b9e-aee1-dbee17616eea.jpg",
+        "data": "C:/Users/Sarah Benabdallah/Documents/GitHub/FSeg/PlateDetection/data/coco128.yaml",
+        "imgsz": (640, 640),
+        "conf_thres": 0.25,
+        "iou_thres": 0.45,
+        "max_det": 1000,
+        "device": '',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        "view_img": False,
+        "save_txt": False,
+        "save_csv": False,
+        "save_conf": False,
+        "save_crop": False,
+        "nosave": False,
+        "classes": None,  # filter by class: --class 0, or --class 0 2 3
+        "agnostic_nms": False,  # class-agnostic NMS
+        "augment": False,  # augmented inference
+        "visualize": False,  # visualize features
+        "update": False,  # update all models
+        "project": "C:/Users/Sarah Benabdallah/Documents/GitHub/FSeg/PlateDetection/runs/detect",
+        "name": "exp",
+        "exist_ok": False,  # existing project/name ok, do not increment
+        "line_thickness": 3,  # bounding box thickness (pixels)
+        "hide_labels": False,  # hide labels
+        "hide_conf": False,  # hide confidences
+        "half": False,  # use FP16 half-precision inference
+        "dnn": False,  # use OpenCV DNN for ONNX inference
+        "vid_stride": 1  # video frame-rate stride
+    }
     main(opt)
 
     end_time = time.time()
 
     print('Elapsed time : ', end_time - start_time)
-
-    
-    
-
-    
-
-
-
-
-

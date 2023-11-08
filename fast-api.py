1b04d9cf093ec9901d3c08bc7d88c8bfcb78ba8e
tahayass <tahayassinetizaoui@insat.u-carthage.tn>
2023-11-08 18:42:18 +0100

    return the image as an url

--- fast-api.py
+++ fast-api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile,Query
+from fastapi.responses import FileResponse
 from PIL import Image
 import os
 import json
@@ -11,10 +12,14 @@ app = FastAPI()
 
 # Directory to save temporary uploaded files
 upload_dir = "uploads"
+transformed_image_dir = "transformed_image"
 
 # Create the directory if it doesn't exist
 if not os.path.exists(upload_dir):
     os.makedirs(upload_dir)
+    
+if not os.path.exists(transformed_image_dir):
+    os.makedirs(transformed_image_dir)
 
 
 @app.post("/uploadfile/")
@@ -66,16 +71,18 @@ async def upload_file(file: UploadFile):
     pixel_count_dict,bbox_dict,packaged_bbox_dict,transformed_image = pipeline(OPT)
 
     # Save the transformed image to a temporary location
-    transformed_image_path = os.path.join(upload_dir, "transformed_" + file.filename)
+    transformed_image_path = os.path.join(transformed_image_dir, "transformed_" + file.filename)
     cv2.imwrite(transformed_image_path, transformed_image)
 
     # Encode the transformed image as Base64
-    with open(transformed_image_path, "rb") as img_file:
-        transformed_image_data = base64.b64encode(img_file.read()).decode("utf-8")
+    #with open(transformed_image_path, "rb") as img_file:
+        #transformed_image_data = base64.b64encode(img_file.read()).decode("utf-8")
+
+    transformed_image_url = f"/get_transformed_image/{file.filename}"
 
     # Create a response JSON that includes the Base64-encoded image, pixel_count, and MIME type
     response_data = {
-        "transformed_image": transformed_image_data,
+        "transformed_image": transformed_image_url,
         "plate_food" : {
             "bounding_boxes": bbox_dict,
             "pixel_count": pixel_count_dict
@@ -88,6 +95,17 @@ async def upload_file(file: UploadFile):
     }
 
     return response_data
+
+@app.get("/get_transformed_image/{filename}")
+async def get_transformed_image(
+    filename: str,
+    content_type: str = Query("image/jpeg", title="Content-Type Header")
+):
+    transformed_image_path = os.path.join(transformed_image_dir, "transformed_" + filename)
+    if os.path.exists(transformed_image_path):
+        return FileResponse(transformed_image_path, headers={"Content-Type": content_type})
+    else:
+        return {"error": "Image not found"}
 
 if __name__ == "__main__":
     import uvicorn
